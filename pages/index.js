599910e8ddbb0e3ed2503d6fb84a2508cf870f61
Siddhant <s1d.official011001011@gmail.com>
2022-11-30 08:36:22 +0530

    Web-App_v1

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React , {useState} from 'react'
+import NavBar from '../components/Navbar/NavBar'
+import TopNavBar from '../components/Navbar/TopNavBar'
+
+export default function index() {
+
+  return (
+       <main>
+     hello founders
+    </main>
+  )
+}
